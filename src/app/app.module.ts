6032146db7e82280d5f37ca504ed4ff5bc294699
karen ayman <karenayman98@gmail.com>
2022-02-20 20:33:40 +0200

    doha&zahraa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ViewComponent } from './view/view.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    NavBarComponent,
+    ViewComponent,
+    AboutComponent,
+    ServicesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
